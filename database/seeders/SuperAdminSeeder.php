87234e78a9fcaf10c9c9f4a95e038e86b2fa203a
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-17 16:21:32 -0600

    asdfasdf

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@thecommunityhelpdesk.org',
+            'password' => Hash::make('4ndr0m3d@'),
+            'role' => 'super_admin',
+            'is_tenant_admin' => false,
+            'status' => 'active',
+            'admin' => 'yes',
+            'usertype' => 'SUPERADMIN',
+            'email_verified_at' => now(),
+        ]);
     }
 }
