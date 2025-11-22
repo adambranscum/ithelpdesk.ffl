5006bf557d7694e6b019788ebae2d0791ff4b528
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-22 16:55:25 -0600

    update

--- app/Http/Middleware/EnsureCentralDomain.php
+++ app/Http/Middleware/EnsureCentralDomain.php
@@ -11,7 +11,7 @@ class EnsureCentralDomain
     public function handle(Request $request, Closure $next): Response
     {
         $currentHost = $request->getHost();
-        $centralDomains = config('tenancy.central_domains', ['127.0.0.1', 'localhost']);
+        $centralDomains = config('tenancy.central_domains', ['www.thecommunityhelpdesk.org', 'thecommunityhelpdesk.org']);
         
         if (!in_array($currentHost, $centralDomains)) {
             abort(404, 'Page not found on this domain');
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
@@ -22,6 +23,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Force URL generation to use APP_URL
+        URL::forceScheme('https');
+        if ($appUrl = config('app.url')) {
+            URL::forceRootUrl($appUrl);
+        }
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
@@ -37,13 +44,12 @@ class RouteServiceProvider extends ServiceProvider
                     ->group(base_path('routes/web.php'));
             }
             
-            // Tenant Routes - match any subdomain
+            // Tenant Routes - WITH tenant middleware
             Route::middleware([
                 'web',
                 InitializeTenancyByDomain::class,
                 PreventAccessFromCentralDomains::class,
-            ])
-            ->group(base_path('routes/tenant.php'));
+            ])->group(base_path('routes/tenant.php'));
         });
     }
 
