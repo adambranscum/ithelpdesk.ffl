22212eb85ed0509da5a3ede42e1cf48427f1e825
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-25 11:55:58 -0600

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,9 +3,6 @@
 <div class="container shadow mt-4 pt-4 pb-4">
     <div class="d-flex">
         <h2 class="mb-4 fw-bolder me-auto text-gradient-primary">Account Settings</h2>
-        <a href="/tickets" class="ms-auto btn btn-outline-primary mb-4">
-            &larr; Back to Tickets
-        </a>
     </div>
 
     <div class="card border-0 shadow-sm mb-4">
