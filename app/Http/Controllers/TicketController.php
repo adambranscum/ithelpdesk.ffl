9c748765cd5d7fba3b96e129fda66c03328d08bb
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-25 01:40:12 -0600

    added model

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -6,6 +6,7 @@ use App\Models\Ticket;
 use App\Models\Device;
 use App\Models\User;
 use App\Models\Software;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\TicketResolved;
