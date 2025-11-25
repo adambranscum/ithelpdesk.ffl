798d6bdba149dc4a30a72051d8605857c8a0037b
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-25 13:28:55 -0600

    Update DeviceStatsController.php

--- app/Http/Controllers/DeviceStatsController.php
+++ app/Http/Controllers/DeviceStatsController.php
@@ -93,11 +93,7 @@ class DeviceStatsController extends Controller
 
         // Average device age by make
         $avgAgeByMake = Device::where('library_uid', $user->library_uid)
-            ->select(
-                'make',
-                DB::raw('AVG(TIMESTAMPDIFF(YEAR, purchased, NOW())) as avg_age'),
-                DB::raw('COUNT(*) as count')
-            )
+            ->selectRaw('make, AVG(TIMESTAMPDIFF(YEAR, purchased, NOW())) as avg_age, COUNT(*) as count')
             ->whereNotNull('purchased')
             ->whereNotNull('make')
             ->where('make', '!=', '')
