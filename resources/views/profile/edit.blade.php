4aa47fccf86ed5aaeec310a1210c7278b023753b
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-25 11:52:09 -0600

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,12 +1,15 @@
 <x-app-layout>
 
-<div class="container shadow mt-4 mb-5 pb-3 pt-2">
-    <a href="/tickets" class="btn btn-outline-secondary mb-3 mt-2">
-        &larr; Back
-    </a>
+<div class="container shadow mt-4 pt-4 pb-4">
+    <div class="d-flex">
+        <h2 class="mb-4 fw-bolder me-auto text-gradient-primary">Account Settings</h2>
+        <a href="/tickets" class="ms-auto btn btn-outline-primary mb-4">
+            &larr; Back to Tickets
+        </a>
+    </div>
 
     <div class="card border-0 shadow-sm mb-4">
-        <div class="card-header bg-info text-dark fw-bold">Change Password</div>
+        <div class="card-header bg-primary text-white fw-bold">Change Password</div>
         <div class="card-body p-4">
             @include('profile.partials.update-password-form')
         </div>
