47056e0a64c071e53ee38060f5d7df94ac9db3d2
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-26 00:23:29 -0600

    switched from tailwind to boostrap

--- resources/views/library/pending-approval.blade.php
+++ resources/views/library/pending-approval.blade.php
@@ -1,6 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
+<x-guest-layout>
+    
 <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full bg-white rounded-lg shadow-md p-8 text-center">
         <div class="mb-6">
@@ -27,4 +26,4 @@
         </a>
     </div>
 </div>
-@endsection
+</x-guest-layout>
