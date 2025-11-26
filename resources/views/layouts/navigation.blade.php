5d123c5210abfce909efec327b85b133256e4156
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-26 00:52:18 -0600

    hamburger menu color change

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,8 @@
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-            style="border: none; padding: 0.25rem 0.75rem;">
-            <span class="navbar-toggler-icon" style="background-image: url('data:image/svg+xml;utf8,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 30 30%22><line x1=%225%22 y1=%227%22 x2=%2225%22 y2=%227%22 stroke=%22url(%23grad1)%22 stroke-width=%223%22 stroke-linecap=%22round%22/><line x1=%225%22 y1=%2215%22 x2=%2225%22 y2=%2215%22 stroke=%22url(%23grad1)%22 stroke-width=%223%22 stroke-linecap=%22round%22/><line x1=%225%22 y1=%2223%22 x2=%2225%22 y2=%2223%22 stroke=%22url(%23grad1)%22 stroke-width=%223%22 stroke-linecap=%22round%22/><defs><linearGradient id=%22grad1%22 x1=%220%25%22 y1=%220%25%22 x2=%22100%25%22 y2=%220%25%22><stop offset=%220%25%22 style=%22stop-color:%239b59b6;stop-opacity:1%22/><stop offset=%22100%25%22 style=%22stop-color:%23e74c3c;stop-opacity:1%22/></linearGradient></defs></svg>'); width: 1.5em; height: 1.5em;"></span>
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
