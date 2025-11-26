99b5e21bbc5f0cda31975dd1d81f77e89d64c2ba
Adam Branscum <93352907+adambranscum@users.noreply.github.com>
2025-11-26 01:09:04 -0600

    css v update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="grad-icon-nav "></span>
+            <span class="grad-icon-nav"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
